83237732b339e0b76d99c4176429862296da6918
mujaheedabdullahi <mujaheed05@gmail.com>
2023-12-16 12:43:23 +0800

    add Day_1_assignment

---
+++ Day_1/helloworld.py
@@ -0,0 +1,22 @@
+print(3 + 4)  # addition(+)
+print(3 - 4)  # subtraction(-)
+print(3 * 4)  # multiplication(*)
+print(3 / 4)  # division(/)
+print(3 ** 4)  # exponential(**)
+print(3 % 4)  # modulus(%)
+print(3 // 4)  # Floor division operator(//)
+print("\n")
+
+# The data types
+
+print(type(10))  # Int
+print(type(3.14))  # Float
+print(type(1 + 3j))  # Complex
+print(type('Mujaheed'))  # String
+print(type([1, 2, 3]))  # List
+print(type({'name': 'Mujaheed'}))  # Dictionary
+print(type({9.8, 3.14, 2.7}))  # Set
+print(type((9.8, 3.14, 2.7)))  # Tuple
+print("\n")
+print("Find an Euclidian distance (2, 3) and (10, 8)")
+print('{0:.2f}'.format(((2 - 10) ** 2 + (3 - 8) ** 2) ** 0.5))
