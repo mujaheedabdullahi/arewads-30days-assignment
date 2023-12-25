14da611d407d3bc7277d8f72e8dd6f2973997182
mujaheedabdullahi <mujaheed05@gmail.com>
2023-12-25 15:28:14 +0800

    Day_4

---
+++ Day_4/strings.py
@@ -0,0 +1,120 @@
+
+# Day 3 Assignment 
+
+# Question  1
+print('Thirty' + ' Days' + ' Of' + ' Python')
+
+# Question  2
+print(' '.join(['Coding', 'For', 'All']))
+
+# Question  3
+company = 'Coding For All'
+
+# Question  4
+print(company)
+
+# Question  5
+print(len(company))
+
+# Question  6
+print(company.upper())
+
+# Question  7
+print(company.lower())
+
+# Question  8
+print(company.capitalize())
+print(company.title())
+print(company.swapcase())
+
+# Question  9
+print(company[7:])
+
+# Question  10
+print(company[company.index('Coding'):8])
+print(company[company.find('Coding'):8])
+
+# Question  11
+print(company.replace('Coding ', 'Not '))
+
+# Question  12
+print('Python For Everyone'.replace('Everyone', 'All'))
+
+# Question  13
+print(company.split())
+
+# Question  14
+print("Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon".split(','))
+
+# Question  15
+print(company[0])
+
+# Question  16
+print(len(company) - 1)
+
+# Question  17
+print(company[10])
+
+# Question  18
+words = "Python For Everyone".split()
+print(words[0][0] + words[1][0] + words[2][0])
+
+# Question  19
+words = 'Coding For All'.split()
+print(words[0][0] + words[1][0] + words[2][0])
+
+# Question  20
+print("Coding For All".index('C'))
+
+# Question  21
+print("Coding For All".index('F'))
+
+# Question  22
+print("Coding For All People".rfind('l'))
+
+# Question  23
+print('You cannot end a sentence with because because because is a conjunction'.find('because'))
+
+# Question  24
+print('You cannot end a sentence with because because because is a conjunction'.rfind('because'))
+
+# Question  25
+print('You cannot end a sentence with because because because is a conjunction'.replace('because because because', ''))
+
+# Question  26
+print('You cannot end a sentence with because because because is a conjunction'.find('because'))
+
+# Question  27
+print('You cannot end a sentence with because because because is a conjunction'.replace('because because because', ''))
+
+# Question  28
+print('Coding For All'.startswith('Coding'))
+
+# Question  29
+print('Coding For All'.endswith('coding'))
+
+# Question  30
+print('   Coding For All      '.rstrip().lstrip())
+
+# Question  31
+print('30DaysOfPython'.isidentifier())
+print('thirty_days_of_python'.isidentifier())
+
+# Question  32
+print('-'.join(['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon']))
+
+# Question  33
+print('I am enjoying this challenge.\nI just wonder what is next.')
+
+# Question  34
+print('Name\tAge\tCountry\tCity\nRishabh\t250\tFinland\tHelsinki')
+
+# Question 35
+print('radius =', 10)
+print('area =', 3.14, '* radius **', 2)
+print('The area of a circle with radius', 10, 'is', 314, 'meters square.')
+
+# Question  36
+print('8 + 6 = 14')
+print('8 - 6 = 2')
+print('8 * 6 = 48\n8 / 6 = 1.33\n8 % 6 = 2\n8 // 6 = 1\n8 ** 6 = 262144')
