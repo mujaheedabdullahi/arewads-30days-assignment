8b5a04e0bba7b1d44c2f386eae31473f79f38d66
mujaheedabdullahi <mujaheed05@gmail.com>
2024-01-01 07:33:34 +0800

    Day_17

---
+++ Day_17/exception.py
@@ -0,0 +1,8 @@
+# Day 17 Assignment 
+
+names = ['Finland', 'Sweden', 'Norway','Denmark','Iceland', 'Estonia','Russia']
+try:
+    *nordic_countries, es, ru = names
+    print(nordic_countries, es, ru)
+except Exception as e:
+    print(e)
